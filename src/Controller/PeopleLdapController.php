3c2e21dafdcd74ed3fa77ad2ace08dc12f5bf355
markewilson <mark.e.wilson@gmail.com>
2026-02-18 12:26:09 -0500

    clean up test ldap data controller

--- src/Controller/PeopleLdapController.php
+++ src/Controller/PeopleLdapController.php
@@ -14,9 +14,11 @@ class PeopleLdapController extends ControllerBase {
    */
   public function content($netid) {
     //$markup = $netid;
-    $info= as_people_ldap_get_netid_ldap($netid);
+    $info = as_people_ldap_get_netid_ldap($netid);
     $markup = $info[0]["cornelleducampusaddress"][0] .'<br />';
-    $markup = $markup. '<a href="mailto:'.$info[0]["mail"][0].'">'.$info[0]["mail"][0].'</a><br />';
+    if (!empty($info[0]['cornelledupublishedemail'][0])) {
+      $markup = $markup. '<a href="mailto:'.$info[0]['cornelledupublishedemail'][0].'">'.$info[0]['cornelledupublishedemail'][0].'</a><br />';
+    }
     return array(
       '#type' => 'markup',
       '#markup' => $this->t('<h1>LDAP Data for '.$netid.'</h1><div class="slides">
